dd39a5da5ccfbfbe58058d76d9277071863f5a94
souRaV-71 <R9770x@outlook.com>
2023-07-06 20:45:57 +0530

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -12,11 +12,11 @@ from src.exception import CustomException
 
 def save_object(file_path, obj):
     try:
-        dir_path = os.path.dirname(file_path)
+        dir_path=os.path.dirname(file_path)
 
-        os.makedirs(dir_path, exist_ok=True)
+        os.makedirs(dir_path,exist_ok=True)
 
-        with open(file_path, "wb") as file_obj:
+        with open(file_path,'wb') as file_obj:
             pickle.dump(obj, file_obj)
 
     except Exception as e:
