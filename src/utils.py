512c17fb7329f95a7cefe07eb433947184f5b558
souRaV-71 <R9770x@outlook.com>
2023-07-06 20:50:32 +0530

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -4,7 +4,7 @@ import sys
 import numpy as np 
 import pandas as pd
 import dill
-import pickle
+import dill
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 
@@ -17,7 +17,7 @@ def save_object(file_path, obj):
         os.makedirs(dir_path,exist_ok=True)
 
         with open(file_path,'wb') as file_obj:
-            pickle.dump(obj, file_obj)
+            dill.dump(obj, file_obj)
 
     except Exception as e:
         raise CustomException(e, sys) # type: ignore
@@ -57,7 +57,7 @@ def load_object(file_path):
     try:
         with open(file_path, "rb") as file_obj:
             # print(file_path)
-            return pickle.load(file_obj)
+            return dill.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys) # type: ignore
