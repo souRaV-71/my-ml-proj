79f734da33f7c88d7d96a034191a977ffdc84b57
souRaV-71 <R9770x@outlook.com>
2023-07-06 20:02:07 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,27 +1,28 @@
+# Useful for creation ml projects as a package (anyone can they install your package)
 from setuptools import find_packages,setup
 from typing import List
 
 HYPEN_E_DOT='-e .'
-def get_requirements(file_path:str)->List[str]:
+def get_requirements(file_path:str) -> List[str]:
     '''
-    this function will return the list of requirements
+    This function will return a list of requirements
     '''
     requirements=[]
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
-        requirements=[req.replace("\n","") for req in requirements]
+        requirements=[req.replace('\n','') for req in requirements]
 
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
-    
     return requirements
 
-setup(
-name='mlproject',
-version='0.0.1',
-author='Krish',
-author_email='krishnaik06@gmail.com',
-packages=find_packages(),
-install_requires=get_requirements('requirements.txt')
 
+# meta data information about the entire project
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='Ritesh',
+    author_email='riteshsingh9827@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
 )
