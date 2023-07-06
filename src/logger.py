e01a4e3b8fba1728a3a5035548b971d363133919
souRaV-71 <R9770x@outlook.com>
2023-07-06 21:12:53 +0530

    Updated logger.py

--- src/logger.py
+++ src/logger.py
@@ -1,24 +1,18 @@
-import logging
-import os
+import logging,os
 from datetime import datetime
 
-LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+LOG_FILE=f'{datetime.now().strftime("%m_%d_%Y_%H_%M_%S")}.log'
+logs_path=os.path.join(os.getcwd(),'logs',LOG_FILE)
 os.makedirs(logs_path,exist_ok=True)
+
 
 LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
 
 logging.basicConfig(
     filename=LOG_FILE_PATH,
-    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO,
-
-
+    format='[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
 )
 
-# if __name__ == '__main__':
-#     try:
-#         a=1/0
-#     except Exception as e:
-#         logging.info("logging has started")
-#         raise CustomException(e,sys)
+# if __name__=='__main__':
+#     logging.info('logging has started')
