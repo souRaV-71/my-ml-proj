8bdca3d911d0d196a6d098ce78aed0894b10f9a9
souRaV-71 <R9770x@outlook.com>
2023-07-09 02:10:12 +0530

    Update predict_pipeline.py

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -1,55 +1,49 @@
-import os
 import sys
 import pandas as pd
+
 from src.exception import CustomException
 from src.utils import load_object
 
 
 class PredictPipeline:
-    def __init__(self):
+    def __init__(self) -> None:
         pass
-
+    
     def predict(self,features):
         try:
-            model_path=os.path.join("artifacts","model.pkl")
-            #   print(os.path.join("artifacts","model.pkl"))
-            preprocessor_path=os.path.join('artifacts','preprocessor.pkl')
-            #   print(os.path.join('artifacts','preprocessor.pkl'))
-            print("Before Loading")
+            model_path='artifacts\model.pkl'
+            preprocessor_path='artifacts\preprocessor.pkl'
             model=load_object(file_path=model_path)
             preprocessor=load_object(file_path=preprocessor_path)
-            print("After Loading")
             data_scaled=preprocessor.transform(features)
             preds=model.predict(data_scaled)
+            
             return preds
-        
         except Exception as e:
-            raise CustomException(e,sys) # type: ignore
-
-
+            raise CustomException(e,sys)
+        
+        
+    
 
 class CustomData:
-    def __init__(  self,
-        gender: str,
-        race_ethnicity: str,
-        parental_level_of_education,
-        lunch: str,
-        test_preparation_course: str,
-        reading_score: int,
-        writing_score: int):
-
+    '''
+    Responsible for mapping all the data points we receive in the front end to the back end.
+    '''
+    def __init__(self,
+                 gender: str,
+                 race_ethnicity: str,
+                 parental_level_of_education: str,
+                 lunch: int,
+                 test_preparation_course: str,
+                 reading_score: int,
+                 writing_score: int) -> None:
+        
         self.gender = gender
-
         self.race_ethnicity = race_ethnicity
-
         self.parental_level_of_education = parental_level_of_education
-
         self.lunch = lunch
-
         self.test_preparation_course = test_preparation_course
-
         self.reading_score = reading_score
-
         self.writing_score = writing_score
 
     def get_data_as_data_frame(self):
@@ -67,5 +61,4 @@ class CustomData:
             return pd.DataFrame(custom_data_input_dict)
 
         except Exception as e:
-            raise CustomException(e, sys) # type: ignore
-
+            raise CustomException(e, sys)
